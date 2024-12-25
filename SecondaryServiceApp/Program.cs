004f8ea5513475dccde1bc5f34b37aa4abd02854
Srky <p.srdjan02@gmail.com>
2024-12-25 20:46:06 +0100

    certificate

--- SecondaryServiceApp/Program.cs
+++ SecondaryServiceApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
@@ -38,4 +38,61 @@ namespace SecondaryServiceApp
     }
 
     
+}
+*/
+
+
+using System;
+using System.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using Contracts;
+using System.ServiceModel.Security;
+
+namespace SecondaryServiceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Postavljanje sigurnosnih parametara za sekundarni server
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9998/WCFSecondaryService";
+
+            // Windows autentifikacija za klijenta
+            binding.Security.Mode = SecurityMode.Transport; // Windows autentifikacija
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            // Dodavanje sertifikata za server
+            binding.Security.Message.ClientCredentialType = MessageCredentialType.Certificate;
+
+            // Kreiranje hosta za sekundarni server
+            ServiceHost host = new ServiceHost(typeof(WCFSecondaryService));
+            host.AddServiceEndpoint(typeof(IWCFSecondaryService), binding, address);
+
+            // Podesavanje autentifikacije između sekundarnog i primarnog servera
+            host.Credentials.ServiceCertificate.SetCertificate(
+                StoreLocation.LocalMachine,
+                StoreName.My,
+                X509FindType.FindBySubjectName,
+                "secondaryserver"); // Sertifikat za sekundarni server
+
+            // Podesavanje ChainTrust validacije
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.ChainTrust;
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            // Otvoriti host
+            host.Open();
+            Console.WriteLine("WCFSecondaryService is opened. Press <enter> to finish...");
+
+            // Replikator za sinhronizaciju podataka sa primarnim serverom
+            using (var replicatorHandler = new ReplicatorClientHandler())
+            {
+                replicatorHandler.GetAllAlarms();
+                Console.ReadLine();
+            }
+
+            host.Close();
+        }
+    }
 }
--- ServiceApp/Program.cs
+++ ServiceApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,4 +46,64 @@ namespace ServiceApp
             host.Close();
 		}
 	}
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using Contracts;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Description;
+using System.Threading;
+
+namespace ServiceApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Postavljanje sigurnosnih parametara za primarni server
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/WCFService";
+            Database db = new Database();
+
+            // Windows autentifikacija za klijenta
+            binding.Security.Mode = SecurityMode.Transport; // Windows autentifikacija
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            // Dodavanje sertifikata za server
+            binding.Security.Message.ClientCredentialType = MessageCredentialType.Certificate;
+
+            // Kreiranje hosta za primarni server
+            ServiceHost host = new ServiceHost(typeof(WCFService));
+            host.AddServiceEndpoint(typeof(IWCFService), binding, address);
+
+            // Podesavanje autentifikacije između primarnog i sekundarnog servera
+            host.Credentials.ServiceCertificate.SetCertificate(
+                StoreLocation.LocalMachine,
+                StoreName.My,
+                X509FindType.FindBySubjectName,
+                "primaryserver"); // Sertifikat za primarni server
+
+            // Podesavanje ChainTrust validacije
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.ChainTrust;
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            // Otvoriti host
+            host.Open();
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+
+            // Replikator za sinhronizaciju podataka sa sekundarnim serverom
+            Replicator replicator = new Replicator();
+            Thread replicatorThread = new Thread(() => replicator.Run());
+            replicatorThread.IsBackground = true;
+            replicatorThread.Start();
+
+            Console.ReadLine();
+            host.Close();
+        }
+    }
 }
